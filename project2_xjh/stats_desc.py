5af3d8d12a181f8751d26a7657cf6948dccd4558
Sendohx <151484757+Sendohx@users.noreply.github.com>
2023-12-06 09:15:17 +0800

    stats_desc.py

--- project2_xjh/stats_desc.py
+++ project2_xjh/stats_desc.py
@@ -13,15 +13,19 @@ def factor_stats(factor_data, factor):
     计算因子历史值的描述统计信息
     :param factor_data: 因子历史值的dataframe
     :param factor: 因子名称
-    :return dict: 包含均值、标准差、中位数、峰度和偏度的字典
+    :return df: 包含均值、标准差、中位数、峰度和偏度的dataframe
     """
     factor_values = factor_data[factor].values
 
     stats = {}
-    stats['mean'] = np.mean(factor_values)
-    stats['std'] = np.std(factor_values)
-    stats['median'] = np.median(factor_values)
-    stats['kurtosis'] = kurtosis(factor_values)
-    stats['skewness'] = skew(factor_values)
+    stats['mean'] = np.round(np.mean(factor_values), decimals=4)  #保留四位小数
+    stats['std'] = np.round(np.std(factor_values),decimals=4)
+    stats['median'] = np.round(np.median(factor_values),decimals=4)
+    stats['kurtosis'] = np.round(kurtosis(factor_values),decimals=4)
+    stats['skewness'] = np.round(skew(factor_values),decimals=4)
+    
+    df = pd.DataFrame.from_dict(stats, orient='index', columns=[factor])
+    df.index.name = 'Stats'
+    df.reset_index(inplace=True)
 
-    return stats
+    return df
