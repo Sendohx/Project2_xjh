e88a18fea3658e7c31a5f88ecb1bbb0cfd47a32a
Sendohx <151484757+Sendohx@users.noreply.github.com>
2023-12-06 09:17:35 +0800

    output_pdf.py

--- project2_xjh/output_pdf.py
+++ project2_xjh/output_pdf.py
@@ -4,52 +4,30 @@
 # @File：output_pdf.py
 # @Software: PyCharm
 
-import io
-import pdfkit
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 
-from visualization import plot_trend, plot_distribution, plot_correlation
+from visualization import visualization
 from stats_desc import factor_stats
 
 
-def generate_report(factor_data, factor):
-    """
-    生成因子检测工具的pdf
-    :param factor_data: 包含因子数据的DataFrame，其中一列为因子历史值
-    :param factor: 因子名称
-    """
-    pdf_output = io.BytesIO()
+def save_plots_to_pdf(dataframe, plots, output_path):
+    with PdfPages(output_path) as pdf:
+        fig, ax = plt.subplots()
+        ax.axis('off')  # Hide the axis
+        table = ax.table(cellText=dataframe.values, colLabels=dataframe.columns, cellLoc='center', loc='center')
+        table.auto_set_font_size(True)
+        table.scale(1, 1.5)
+        pdf.savefig(fig)
+        
+        for plot in plots:
+            pdf.savefig(plot, bbox_inches = 'tight',pad_inches = 0.5, dpi=3000)
 
-    with PdfPages(pdf_output) as pdf:
-        # 描述统计结果
-        stats = factor_stats(factor_data, factor)
-        pdf.savefig(figure=plt.figure())
-        for key, value in stats.items():
-            plt.text(0.1, 0.9, f"{key}: {value}", transform=plt.gca().transAxes, fontsize=12, va='top')
-            pdf.savefig()
-
-        # 可视化结果
-        plot_trend(factor_data, factor)
-        pdf.savefig()
-        plt.close()
-
-        plot_distribution(factor_data, factor)
-        pdf.savefig()
-        plt.close()
-
-        plot_correlation(factor_data)
-        pdf.savefig()
-        plt.close()
-
-        pdf_output.seek(0)
-        pdfkit.from_file(pdf_output, 'C:/Users/Sendoh/PycharmProjects/project2_xjh' + f'{factor}_report.pdf')
-
-    if __name__ == '__main__':
-        # 样例
-        dict = {}
-        df = pd.DataFrame(dict)
-        factor = 'xxx'
-
-        generate_report(df, factor)
+if __name__ == '__main__':
+    # 样例
+    factor_data = []
+    output_path = "C:/Users/Sendoh/PycharmProjects/" + factor + ".pdf"
+    df = factor_stats(factor_data, factor)
+    plots = visualization(factor_data, factor, factor2)
+    save_plots_to_pdf(df, plots, output_path)
